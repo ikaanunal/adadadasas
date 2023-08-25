11f9921df85ab4ef4e1633bf0e20b3bc0285eed7
Aphrodite <65611915+ikaanunal@users.noreply.github.com>
2023-08-25 14:33:58 +0300

    asdaas
    asasasass

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "ODE3NzE0NjQzNjc3ODcyMTQ4.G7chiI.hz1I099xsMecGHwAyxIhEkN6jIYmO6kKRiGdAw",  // lrows token
+    PREFIX: process.env.PREFIX || ":", // lrows prefix
+    SAHİP: process.env.SAHİP || "645942306163392522", //lrows id
+}
